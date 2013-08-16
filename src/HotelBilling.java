f3314dbd6eed7e38646b0ba733896ff015f82853
hsoJosh <joshua_eh@hotmail.com>
2013-08-16 09:50:35 -0400

    This was pretty wicked. A good brain teaser for the morning.

--- src/HotelBilling.java
+++ src/HotelBilling.java
@@ -1,21 +1,51 @@
+import javax.swing.JOptionPane;
+
 
 public class HotelBilling {
+	
+	final static float SALES_TAX = 0.13f;
+		
+		public static void main(String[] args) {
+		
+			double roomPrice = 97.00;
+			int roomsBook = Integer.parseInt(JOptionPane.showInputDialog(null, "How many rooms did you want? "));
+			double couponRate = 0.10;
+		
+			double totalBill = calculateBill(roomPrice, roomsBook, couponRate);
 
-	public static void main(String[] args) {
-		/*
-		 * In the main method, declare and initialize variables
-		 * for room price, number of rooms booked, and coupon rate.
-		 * 
-		 * Then call each case of the overloaded method 
-		 * calculateBill() to demonstrate the use of overloaded 
-		 * methods
-		 */
+	
+			JOptionPane.showMessageDialog(null, "Your total is $ " + totalBill);
 	}
 
-	/*  
-	 * Here you will create four new methods:
-	 * -- Three cases of the overloaded method calculateBill()
-	 * -- A method called twoDecimalRounding() which will
-	 * 		be called by each case of the calculateBill() method.
-	 */
+	
+		public static double calculateBill(double price) {
+			double finalRate = price * (1 + SALES_TAX); 
+			finalRate = twoDecimalRounding(finalRate);
+			return finalRate;				
+	}
+	
+		public static double calculateBill(double price, double book){
+			double finalRate = price * book;
+			finalRate = finalRate * (1 + SALES_TAX);
+			finalRate = twoDecimalRounding(finalRate);
+			return finalRate;
+	}
+		
+		public static double calculateBill(double price, double roomsBook, double couponRate) {
+			double finalRate = roomsBook * price;
+			finalRate = finalRate * (1 - couponRate);
+			finalRate = finalRate * (1 + SALES_TAX);
+			finalRate = twoDecimalRounding(finalRate);
+			return finalRate;	
+	}
+	
+		public static double twoDecimalRounding(double decimal){
+			decimal = decimal *100;
+			decimal = decimal + 0.5;
+			decimal = (int) decimal;
+			decimal = decimal / 100;
+			return decimal;
+	}
 }
+	
+
